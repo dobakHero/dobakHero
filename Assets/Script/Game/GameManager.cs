1272f65514e2675cd17541b595469cea2e1f47bc
Seobok <Seobok@users.noreply.github.com>
2023-06-28 20:51:58 +0900

    Import Asset
    Import Asset

--- Assets/Script/Game/GameManager.cs
+++ Assets/Script/Game/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -14,6 +15,9 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            
+            DataInitialize();
+            LoadData();
         }
         else if (Instance != this)
         {
@@ -36,7 +40,19 @@ public class GameManager : MonoBehaviour
             var res = _power;
             
             //스트레스로 인한 전투력 감소
-            var stressBuff = (int)(_power * 0.5) * Stress;
+            var stressBuff = (int)(_power * 0.005) * Stress;
+            
+            //몸살로 인한 전투력 감소
+            if (AppliedEvent.ContainsKey(1))
+            {
+                //전투력 계산 순서고려
+            }
+            
+            //열정으로 인한 전투력 증가
+            if (AppliedEvent.ContainsKey(2))
+            {
+                
+            }
 
             return res - stressBuff;
         }
@@ -97,6 +113,17 @@ public class GameManager : MonoBehaviour
         get => _stress;
         set
         {
+            //우울증
+            if (AppliedEvent.ContainsKey(8))
+            {
+                var increaseStress = value - _stress;
+
+                if (increaseStress > 0)
+                {
+                    value = (int)(increaseStress * 1.5) + _stress;
+                }
+            }
+            
             //최댓값
             if (value > 100)
             {
@@ -112,35 +139,42 @@ public class GameManager : MonoBehaviour
 
     #region Event
 
-    private EDay _day = EDay.Monday;
-    public EDay Day
-    {
-        get => _day;
-        set
-        {
-            _day = value;
-            
-            if (_day == EDay.Sunday)
-            {
-                //세금
-            }
-
-            if (_day == EDay.Saturday)
-            {
-                //복권
-            }
-        }
-    }
+    public EDay Day { get; set; } = EDay.Monday;
 
     public void NextDay()
     {
-        if (_day != EDay.Sunday)
+        //요일 변경
+        if (Day != EDay.Sunday)
         {
-            _day += 1;
+            Day += 1;
         }
         else
         {
-            _day = EDay.Monday;
+            Day = EDay.Monday;
+        }
+        
+        //일요일 이벤트
+        if (Day == EDay.Sunday)
+        {
+            //세금
+            var tempChip = 1;
+
+            var tax = (int)(tempChip * 0.3 + Gold * 0.1);
+
+            //증세
+            if (AppliedEvent.ContainsKey(3))
+            {
+                tax = (int)(tax * EventList[3].BuffAmount);
+                AppliedEvent.Remove(3);
+            }
+
+            Gold -= tax;
+        }
+        
+        //토요일 이벤트
+        if (Day == EDay.Saturday)
+        {
+            //복권
         }
         
         //포만감 감소
@@ -151,6 +185,61 @@ public class GameManager : MonoBehaviour
         if (randomValue <= 0.1f) //10%
         {
             DayEvent();
+        }
+
+        foreach (var item in AppliedEvent)
+        {
+            if (item.Value == 0)
+            {
+                //버프 아이콘 제거
+                AppliedEvent.Remove(item.Key);
+            }
+            else
+            {
+                AppliedEvent[item.Key] = item.Value - 1;
+
+                switch (item.Key)
+                {
+                    case 1: //몸살
+                        //GameManager.cs 45줄
+                        break;
+                    case 2: //열정
+                        //GameManager.cs 51줄
+                        break;
+                    case 3: //증세
+                        //GameManager.cs 153줄
+                        break;
+                    case 4: //폐쇄
+                        
+                        break;
+                    case 5: //행운
+                        
+                        break;
+                    case 6: //단속
+                        
+                        break;
+                    case 7: //벌금
+                        Gold -= (int)(Gold * EventList[7].BuffAmount);
+                        AppliedEvent.Remove(7);
+                        break;
+                    case 8: //우울증
+                        //GameManager.cs 116줄
+                        break;
+                    case 9: //장염
+                        break;
+                    case 10://기쁨
+                        Stress -= 15;
+                        AppliedEvent.Remove(10);
+                        break;
+                    case 11://불행
+                        Stress += 15;
+                        AppliedEvent.Remove(11);
+                        break;
+                    default:
+                        Debug.Log("Item CODE ERROR!!! Item.Key : " + item.Key);
+                        break;
+                }
+            }
         }
     }
 
@@ -170,6 +259,19 @@ public class GameManager : MonoBehaviour
     }
 
     #endregion
+
+    #region Gambling
+
+    private int _gamblingMaxCount = 3;
+    private int _gamblingCurrentCount = 3;
+
+    #endregion
+
+    private void DataInitialize()
+    {
+        //AppliedEvent Assignment
+        AppliedEvent = new Dictionary<int, int>();
+    }
 
     public void LoadData()
     {
